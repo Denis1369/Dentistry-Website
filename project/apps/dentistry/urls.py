96ec853c70fc0f3564b22486acc3e0b56174d0fa
Никита Чернов <sercherufa@gmail.com>
2025-10-27 10:55:30 +0500

    Небольшие изменения в маршрутизации

--- project/apps/dentistry/urls.py
+++ project/apps/dentistry/urls.py
@@ -3,10 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ServiceView, FeedbackView, ProfessionView, WorkersView
 
 router = DefaultRouter()
-router.register(r'user', UserViewSet, basename='user')
+router.register(prefix='user', viewset=UserViewSet, basename='user')
 
 urlpatterns = [
-    path('user/', include(router.urls)),
+    path('', include(router.urls)),
     path('services/', ServiceView.as_view(), name='services'),
     path('feedback/', FeedbackView.as_view(), name='feedback'),
     path('profession/', ProfessionView.as_view(), name='profession'),
